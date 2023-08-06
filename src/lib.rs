4798ea78c5f48098a19ee2500bf541dca08348b6
Valentin Golev <v.golev@gmail.com>
2023-08-06 16:09:31 +0200

    kk

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,31 @@
 #![feature(result_option_inspect)]
+#![feature(doc_cfg)]
 
-pub use anyhow::*;
+pub use anyhow::{anyhow, Ok, Result};
 
 #[cfg(feature = "dioxus")]
+#[doc(cfg(dioxus))]
 pub mod dioxus;
 pub mod enums;
 
 #[cfg(feature = "google")]
+#[doc(cfg(google))]
 pub mod google;
 
 #[cfg(feature = "gpt")]
+#[doc(cfg(gpt))]
 pub mod gpt;
 
 pub mod json;
 
 #[cfg(feature = "process")]
+#[doc(cfg(process))]
 pub mod process;
 
 #[cfg(feature = "streams")]
+#[doc(cfg(streams))]
 pub mod streams;
 
 #[cfg(feature = "teloxide")]
+#[doc(cfg(teloxide))]
 pub mod teloxide;
